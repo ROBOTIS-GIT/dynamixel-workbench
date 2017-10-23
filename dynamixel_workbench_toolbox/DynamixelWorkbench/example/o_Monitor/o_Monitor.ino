e350575e23e06c7068785628fac97ac85a577b0f
Taehoon Lim <thlim@robotis.com>
2017-10-24 00:43:15 +0900

    add monitor example

--- dynamixel_workbench_toolbox/DynamixelWorkbench/example/o_Monitor/o_Monitor.ino
+++ dynamixel_workbench_toolbox/DynamixelWorkbench/example/o_Monitor/o_Monitor.ino
@@ -23,29 +23,137 @@
 #define DXL_BUS_SERIAL3 "3"            //Dynamixel on Serial3(USART3)  <-OpenCM 485EXP
 #define DXL_BUS_SERIAL4 "/dev/ttyUSB0" //Dynamixel on Serial3(USART3)  <-OpenCR
 
-#define BAUDRATE  57600
-#define DXL_ID    1
+String cmd[5];
 
 DynamixelWorkbench dxl_wb;
+uint8_t get_id[16];
+uint8_t dxl_cnt = 0;
 
 void setup() 
 {
   Serial.begin(57600);
   while(!Serial);
 
-  dxl_wb.begin(DXL_BUS_SERIAL3, BAUDRATE);
-  dxl_wb.ping(DXL_ID);
-
-  dxl_wb.jointMode(DXL_ID);
+  Serial.println("---------------------------------");
+  Serial.println("Set portHandler First");
+  Serial.println("---------------------------------");
+  Serial.println("port (BAUD)");
+  Serial.println("scan");
+  Serial.println("ping   (ID)");
+  Serial.println("id     (ID) (NEW_ID)");
+  Serial.println("baud   (ID) (NEW_BAUD)");
+  Serial.println("joint  (ID) (GOAL_POSITION)");
+  Serial.println("wheel  (ID) (GOAL_VELOCITY)");
+  Serial.println("reboot (ID) ");
+  Serial.println("reset  (ID) ");
+  Serial.println("---------------------------------");
 }
 
 void loop() 
 {
-  dxl_wb.goalPosition(DXL_ID, 0);
+  if (Serial.available())
+  {
+    String read_string = Serial.readStringUntil('\n');
+
+    read_string.trim();
+
+    split(read_string, ' ', cmd);
+
+    if (cmd[0] == "port")
+    {
+      uint32_t baud = cmd[1].toInt();
+      dxl_wb.begin(DXL_BUS_SERIAL3, baud);
+    }
+    else if (cmd[0] == "scan")
+    {      
+      dxl_cnt = dxl_wb.scan(get_id);
+    }
+    else if (cmd[0] == "ping")
+    {
+      uint8_t id = cmd[1].toInt();
+
+      dxl_wb.ping(id);
+    }
+    else if (cmd[0] == "id")
+    {
+      uint8_t id     = cmd[1].toInt();
+      uint8_t new_id = cmd[2].toInt();
+
+      if (dxl_wb.setID(id, new_id))
+        Serial.println("Succeed to change ID");
+      else
+        Serial.println("Failed");
+
+    }
+    else if (cmd[0] == "baud")
+    {
+      uint8_t  id       = cmd[1].toInt();
+      uint32_t new_baud = cmd[2].toInt();
+
+      if (dxl_wb.setBaud(id, new_baud))
+        Serial.println("Succeed to change BaudRate");
+      else
+        Serial.println("Failed");
+    }
+    else if (cmd[0] == "joint")
+    {
+      uint8_t id    = cmd[1].toInt();
+      uint16_t goal = cmd[2].toInt();
+
+      dxl_wb.jointMode(id);
+      if (dxl_wb.goalPosition(id, goal))
+       Serial.println("Succeed!!");
+      else
+        Serial.println("Failed");
+    }
+    else if (cmd[0] == "wheel")
+    {
+      uint8_t id    = cmd[1].toInt();
+      int32_t goal = cmd[2].toInt();
+
+      dxl_wb.wheelMode(id);
+      if (dxl_wb.goalSpeed(id, goal))
+        Serial.println("Succeed!!");
+      else
+        Serial.println("Failed");
+    }
+    else if (cmd[0] == "reboot")
+    {
+      uint8_t id = cmd[1].toInt();
+
+      dxl_wb.reboot(id);
+    }
+    else if (cmd[0] == "reset")
+    {
+      uint8_t id = cmd[1].toInt();
+
+      dxl_wb.reset(id);
+    }
+  }
+}
+
+void split(String data, char separator, String* temp)
+{
+	int cnt = 0;
+	int get_index = 0;
+
+  String copy = data;
   
-  delay(2000);
+	while(true)
+	{
+		get_index = copy.indexOf(separator);
 
-  dxl_wb.goalPosition(DXL_ID, 2000);
+		if(-1 != get_index)
+		{
+			temp[cnt] = copy.substring(0, get_index);
 
-  delay(2000);
+			copy = copy.substring(get_index + 1);
+		}
+		else
+		{
+      temp[cnt] = copy.substring(0, copy.length());
+			break;
+		}
+		++cnt;
+	}
 }
