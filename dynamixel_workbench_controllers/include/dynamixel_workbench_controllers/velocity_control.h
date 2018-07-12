50a09431a57d720eaa5f2f3c0eaf72baa37fcfcc
Kai <mezlxx@gmail.com>
2018-07-12 11:17:24 +0800

    add support for odom

--- dynamixel_workbench_controllers/include/dynamixel_workbench_controllers/velocity_control.h
+++ dynamixel_workbench_controllers/include/dynamixel_workbench_controllers/velocity_control.h
@@ -37,11 +37,26 @@
 
 #include "message_header.h"
 #include <geometry_msgs/Twist.h>
+#include <std_msgs/Empty.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
 
+#include <cmath>
 #include <dynamixel_workbench_toolbox/dynamixel_workbench.h>
 #include <dynamixel_workbench_msgs/DynamixelStateList.h>
 #include <dynamixel_workbench_msgs/WheelCommand.h>
+#define WHEEL_NUM                        2
+#define LEFT                             0
+#define RIGHT                            1
 
+#define LINEAR                           0
+#define ANGULAR                          1
+
+#define DEG2RAD(x)                       (x * 0.01745329252)  // *PI/180
+#define RAD2DEG(x)                       (x * 57.2957795131)  // *180/PI
+
+#define TICK2RAD                         0.001533981  // 0.087890625[deg] * 3.14159265359 / 180 = 0.001533981f
 class VelocityControl
 {
  private:
@@ -52,9 +67,11 @@ class VelocityControl
 
   // ROS Topic Publisher
   ros::Publisher dynamixel_state_list_pub_;
+  ros::Publisher odom_pub_;
 
   // ROS Topic Subscriber
   ros::Subscriber cmd_vel_sub_;
+  ros::Subscriber reset_odom_sub_;
 
   // ROS Service Server
   ros::ServiceServer wheel_command_server_;
@@ -70,6 +87,19 @@ class VelocityControl
   float wheel_separation_;
   float wheel_radius_;
 
+  // Calculation for odometry
+  bool init_encoder = true;
+  double  last_diff_tick[WHEEL_NUM] = {0.0, 0.0};
+  double  last_rad[WHEEL_NUM]       = {0.0, 0.0};
+  double  last_velocity[WHEEL_NUM]  = {0.0, 0.0};
+  /// Current timestamp:
+  ros::Time timestamp_;
+  float odom_pose[3];
+  double odom_vel[3];
+  nav_msgs::Odometry odom;
+  geometry_msgs::TransformStamped odom_tf;
+  tf::TransformBroadcaster tf_broadcaster;
+
  public:
   VelocityControl();
   ~VelocityControl();
@@ -81,11 +111,16 @@ class VelocityControl
   void initPublisher();
   void initSubscriber();
   void dynamixelStatePublish();
+  void initOdom(void);
+  void odomPublish();
+  void updateMotorInfo(double left_tick, double right_tick);
+  bool calcOdometry(const ros::Time &time);
 
   void initServer();
   bool wheelCommandMsgCallback(dynamixel_workbench_msgs::WheelCommand::Request &req,
                                dynamixel_workbench_msgs::WheelCommand::Response &res);
   void commandVelocityCallback(const geometry_msgs::Twist::ConstPtr &msg);
+  void subscribeResetOdometry(const std_msgs::EmptyConstPtr msg);
 };
 
 #endif //DYNAMIXEL_WORKBENCH_VELOCITY_CONTROL_H
